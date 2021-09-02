3ba0a1fbdf6f69e120a59f89583eae4fd3f7f299
Adriele A Secundino <dri.leela@gmail.com>
2021-09-02 16:20:43 -0300

    chore: packge.json and change queue data

--- packages/resync-mailchimp/src/index.ts
+++ packages/resync-mailchimp/src/index.ts
@@ -20,11 +20,11 @@ const config = {
   port: params.port,
   database: params.pathname.split('/')[1]
 };
-console.log({ config });
+
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 3003;
-console.log(PORT);
+const PORT = process.env.PORT || 3000;
+console.log(`Porta: ${PORT}`);
 
 const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 const queueContacts = new Queue("contacts", REDIS_URL);
@@ -33,8 +33,7 @@ app.post('/resync', async (req, res) => {
 
   // get request input
   const { iscommunity, id } = req.body.input;
-  //ae
-  //const list: ContactOutput[] = await queryContacts(widget_id);
+ 
   //ae
   const pool = new Pool(config);
   //ae
@@ -52,48 +51,47 @@ app.post('/resync', async (req, res) => {
                                     :`select id, kind from widgets where id = ${id}`);
 
   //ae
-
-  console.log(queryWidget);
   const widgets = await pool.query(queryWidget).then((result) =>{return result.rows});  
+  
   widgets.forEach((w) => {                     
-  let table;   
-  switch(w.kind) { 
-    case 'donation': { 
-      table = 'donations'; 
-      break; 
+    let table;   
+    switch(w.kind) { 
+      case 'donation': { 
+        table = 'donations'; 
+        break; 
+      } 
+      case 'form': { 
+        table = 'form_entries';
+        break; 
+      } 
+      case 'pressure': {
+        table = 'activist_pressures';
+        break;
+      }
+      case 'pressure-phone': {
+        table = 'activist_pressures';
+        break;
+      }        
+      default: { 
+        table = 'form_entries'
+        break; 
+      } 
     } 
-    case 'form': { 
-      table = 'form_entries';
-      break; 
-    } 
-    case 'pressure': {
-      table = 'activist_pressures';
-      break;
-    }
-    case 'pressure-phone': {
-      table = 'activist_pressures';
-      break;
-    }        
-    default: { 
-      table = 'form_entries'
-      break; 
-    } 
-  } 
-  const query = new QueryStream(`select
-        --a.id activist_id,
+    const query = new QueryStream(`select
+        a.id activist_id,
         a.email activist_email, a.first_name
-        --, a.last_name,
-        --a.phone,a.city,a.state,      
-        --w.id widget_id, 
-        --w.kind widget_kind,
-        --b.id as block_id,
-        --m.id mobilization_id , 
-        --m."name" mobilization_name,
-        --c.id community_id, 
-        --c."name" community_name, 
-        --c.mailchimp_api_key , 
-        ---c.mailchimp_list_id ,
-        --t.*
+        , a.last_name,
+        a.phone,a.city,a.state,      
+        w.id widget_id, 
+        w.kind widget_kind,
+        b.id as block_id,
+        m.id mobilization_id , 
+        m."name" mobilization_name,
+        c.id community_id, 
+        c."name" community_name, 
+        c.mailchimp_api_key , 
+        c.mailchimp_list_id ,
+        t.*
         from
         activists a,${table} t 
           left join widgets w on t.widget_id = w.id
@@ -103,38 +101,41 @@ app.post('/resync', async (req, res) => {
         where a.id = t.activist_id  and w.id = ${w.id}
         -- and fe.mailchimp_syncronization_at is null 
         order by a.id asc 
-     `);
-  const stream =  client.query(query);
-  stream.on('end',async () => {
-    console.log("fim", await queueContacts.getJob(await queueContacts.count())); 
-    return res.json("OK");
-    
-  }); 
-  stream.on('error', (err: any) => { console.log("Erro",err)});
- // stream.on('data', (data : any) => {
-  //  console.log(data);
- // })  ;
-                                  
-  stream.pipe(JSONStream.stringify()).pipe(
-    es.map((data: any, callback:any) => { 
-      let add = async (data: any) => {
-        return await  queueContacts.add( { data }, {
-          removeOnComplete: true,
-        });
-      }
-      add(data)
-      .then((data) => {
-        callback(null, JSON.stringify(data));
-      })
-      .catch((err) => {
-        console.log(`ERROR ADD QUEUE: ${err}`);
-      });
-    }));
-  
-});
- 
- // return res.json("OK");
+    `);
 
+    const stream =  client.query(query);
+    stream.on('end',async () => {
+      console.log("fim",await queueContacts.getJob(await queueContacts.count())); 
+      //wmc
+      return res.json({
+        queue: await queueContacts.getJobCounts()
+      });
+    
+    }); 
+    stream.on('error', (err: any) => { console.log("Erro",err)});
+                                  
+    stream.pipe(JSONStream.stringify())
+    .pipe(JSONStream.parse("*"))      
+    .pipe(
+      es.map((data:any, callback:any) => { 
+        let add = async (data: any) => {
+          const contact = { 
+                            first_name: data.first_name, 
+                            email: data.activist_email 
+                          }
+          return await  queueContacts.add( { contact }, {
+            removeOnComplete: true,
+          });
+        }
+        add(data)
+        .then((data) => {
+          callback(null, JSON.stringify(data));
+        })
+        .catch((err) => {
+          console.log(`ERROR ADD QUEUE: ${err}`);
+        });
+    }));
+  });
 });
 
 app.listen(PORT);
