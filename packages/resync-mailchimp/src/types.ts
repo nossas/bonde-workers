b6336eaee97584ef50fc2493165c746b4cf40d79
Adriele A Secundino <dri.leela@gmail.com>
2021-09-03 18:04:45 -0300

    chore: adapt mailchimp add tags

---
+++ packages/resync-mailchimp/src/types.ts
@@ -0,0 +1,22 @@
+export type Contact= {
+    id: number; 
+    email: string,
+    first_name: string,
+    last_name: String,
+    phone: string,
+    city: string,
+    state: string,
+    community_id: number,
+    community_name: string,
+    mobilization_id: number,
+    mobilization_name: string,
+    widget_id: number,
+    kind: string,
+    mailchimp_api_key: string,
+    mailchimp_list_id: string
+}
+  
+export type Tag = {
+  name: string
+  status: string
+}
