8a231df4a3f9ad80888a7299ac1d9b8883fdac49
Adriele A Secundino <dri.leela@gmail.com>
2021-09-13 09:25:14 -0300

    chore: orginize test

--- packages/resync-mailchimp/__test__/resync-mailchimp.spec.ts
+++ packages/resync-mailchimp/__test__/resync-mailchimp.spec.ts
@@ -3,13 +3,9 @@ import pgmock, { getPool } from 'pgmock2';
 import Redis from "ioredis-mock";
 import Queue from "bull";
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
-
 const pg = new pgmock();
 const pool = getPool(pg);
-pg.add('select id, kind from widgets where id = 1234', ['number'], {
+pg.add('select id, kind from widgets where id = 1234', ['string'], {
   rowCount: 1,
   rows: [
       { id: 1234, kind: 'donation' }
@@ -23,13 +19,17 @@ const mockQueue = new Queue("contacts-mailchimp", { createClient: () => redisMoc
 const client =  pool.connect();
 utils.dbClient.mockResolvedValue(client);
 utils.queueContacts = mockQueue;
+const spyonQuery = jest.spyOn(pg,"query" );
 
-describe("resyncMailchimpHandle", () => {
+describe("resyncMailchimpHandle search with widget", () => {
 
   it("should return id queue", async()=>{
-    const spyonQuery = jest.spyOn(pg,"query" );
     const id = await resyncMailchimpHandle(1234,false);
     expect(id).toBe("bull:contacts-mailchimp:id");
-    expect(spyonQuery).toBeCalledTimes(2);
   });
+
+  it("should call query 2 times", ()=>{ 
+    expect(spyonQuery).toHaveBeenCalledWith('select id, kind from widgets where id = 1234', undefined);
+    expect(spyonQuery).toBeCalledTimes(2);
+  });  
 ;})
