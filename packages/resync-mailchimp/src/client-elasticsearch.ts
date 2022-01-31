672d9313c8c5bc4d22271ef4065e31ffd32fda31
Adriele A Secundino <dri.leela@gmail.com>
2022-01-31 09:37:48 -0300

    chore(resync-mailchimp): change default values for elasticsearch client

--- packages/resync-mailchimp/src/client-elasticsearch.ts
+++ packages/resync-mailchimp/src/client-elasticsearch.ts
@@ -1,13 +1,14 @@
 import { Client } from '@elastic/elasticsearch';
 import dotenv from "dotenv";
 dotenv.config();
+
 export const clientES =
     new Client({
     cloud: {
-      id: process.env.ELASTICSEARCH_CLOUD_ID || 'http://localhost:9200'
+      id: process.env.ELASTICSEARCH_CLOUD_ID || 'name:bG9jYWxob3N0JGFiY2QkZWZnaA=='
     },
     auth: {
       username: "elastic",
-      password: process.env.ELASTICSEARCH_PASSWORD || ''
+      password: process.env.ELASTICSEARCH_PASSWORD || 'changeme'
     }
   });
